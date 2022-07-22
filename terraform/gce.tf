08438f80a155b327cdcf4c47897aa68cccf0920b
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-07-22 22:01:14 +0100

    Fix terraform file formatting
    It prevents the terraform from being applied on merge.

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -90,7 +90,7 @@ resource "google_compute_instance" "neo4j" {
     network = "default"
     access_config {
       network_tier = "STANDARD"
-      nat_ip = google_compute_address.neo4j.address
+      nat_ip       = google_compute_address.neo4j.address
     }
   }
 
