29a29b196ad98b6ad4d9b87e47cb633c775fc562
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-07-22 23:32:01 +0100

    Allow neo4j service account to download image

--- terraform/artifact-repository.tf
+++ terraform/artifact-repository.tf
@@ -15,11 +15,20 @@ resource "google_service_account" "artifact_registry_docker" {
   description  = "Service account for pushing docker images"
 }
 
-resource "google_artifact_registry_repository_iam_member" "member" {
+resource "google_artifact_registry_repository_iam_member" "artifact_registry_docker" {
   provider   = google-beta
   project    = google_artifact_registry_repository.docker.project
   location   = google_artifact_registry_repository.docker.location
   repository = google_artifact_registry_repository.docker.name
   role       = "roles/artifactregistry.writer"
   member     = "serviceAccount:${google_service_account.artifact_registry_docker.email}"
+}
+
+resource "google_artifact_registry_repository_iam_member" "compute_engine_neo4j" {
+  provider   = google-beta
+  project    = google_artifact_registry_repository.docker.project
+  location   = google_artifact_registry_repository.docker.location
+  repository = google_artifact_registry_repository.docker.name
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${google_service_account.gce_neo4j.email}"
 }
