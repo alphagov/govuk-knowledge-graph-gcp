6d1fb349b0329e2d902e89344ed8c04a37dbfcea
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-07-22 23:02:25 +0100

    Configure egress rule as actually egress (#38)

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -44,8 +44,9 @@ resource "google_compute_firewall" "neo4j-ingress" {
 }
 
 resource "google_compute_firewall" "neo4j-egress" {
-  name    = "firewall-neo4j-egress"
-  network = google_compute_network.default.name
+  name      = "firewall-neo4j-egress"
+  network   = google_compute_network.default.name
+  direction = "egress"
 
   allow {
     protocol = "tcp"
