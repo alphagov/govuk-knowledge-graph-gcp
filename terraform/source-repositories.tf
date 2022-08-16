1f55450282a8f88de199986abe5a0503b533ee2b
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-08-16 16:23:52 +0100

    Push main branch to google source repositories
    GitHub Action to push the main branch to Google Cloud Source
    Repositories on merge, using Workload Identity Federation (keyless
    authentication). This makes the repository accessible to the Google
    Cloud Project without having to maintain keys.
    
    First step.  A subsequent step will uncomment the service account in
    `workload-identity-federation.tf`.

---
+++ terraform/source-repositories.tf
@@ -0,0 +1,18 @@
+# For GitHub Actions to push the repository to GCP
+
+resource "google_sourcerepo_repository" "alphagov_govuk_knowledge_graph_gcp" {
+  name = "alphagov/govuk-knowledge-graph-gcp"
+}
+
+resource "google_service_account" "source_repositories_github" {
+  account_id   = "source-repositories-github"
+  display_name = "Source Repositories Service Account for GitHub"
+  description  = "Service account for pushing git repositories from GitHub Actions"
+}
+
+resource "google_sourcerepo_repository_iam_member" "member" {
+  project    = google_sourcerepo_repository.alphagov_govuk_knowledge_graph_gcp.project
+  repository = google_sourcerepo_repository.alphagov_govuk_knowledge_graph_gcp.name
+  role       = "roles/writer"
+  member     = "serviceAccount:${google_service_account.source_repositories_github.email}"
+}
