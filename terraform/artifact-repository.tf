b1d63097c0e31c80b62f5e0f3069677b843c9b18
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-08-23 02:15:30 +0100

    Set the type of account

--- terraform/artifact-repository.tf
+++ terraform/artifact-repository.tf
@@ -30,5 +30,5 @@ resource "google_artifact_registry_repository_iam_member" "docker_reader" {
   location   = google_artifact_registry_repository.docker.location
   repository = google_artifact_registry_repository.docker.name
   role       = "roles/artifactregistry.reader"
-  member     = google_service_account.gce_mongodb.email
+  member     = "serviceAccount:${google_service_account.gce_mongodb.email}"
 }
