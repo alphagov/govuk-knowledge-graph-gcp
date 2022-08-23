c2c624ceac6d5730a14a8b77a7bd1f2f864c0281
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-08-23 04:27:19 +0100

    Run mongod in the background

--- docker/mongodb/run.sh
+++ docker/mongodb/run.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+
+# turn on bash's job control
+set -m
+
+# Start mongo and put it in the background
+mongod &
+
+# Wait for mongo to start
+sleep 5
+
 # Restore the content_store from its backup .bson file in GCP Storage
+
 # https://stackoverflow.com/questions/6575221
 gsutil cat gs://govuk-knowledge-graph-content-store/mongo.tar.gz \
   | tar xzvO var/lib/mongodb/backup/mongodump/content_store_production/content_items.bson \
@@ -11,3 +23,6 @@ gsutil cat gs://govuk-knowledge-graph-repository/src/mongodb/prepare-content-sto
 # Download from the content store and intermediate datasets, upload to storage
 gsutil cat gs://govuk-knowledge-graph-repository/src/mongodb/extract-from-mongodb.sh \
   | bash
+
+# Bring the background process back into the foreground and leave it there
+fg %1
