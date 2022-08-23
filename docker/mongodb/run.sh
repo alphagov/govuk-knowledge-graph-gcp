bbabf519ccaac1a378aba3c4dffa9c703de6a284
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-08-23 20:32:11 +0100

    Pipe mongo script into mongo not bash

--- docker/mongodb/run.sh
+++ docker/mongodb/run.sh
@@ -20,8 +20,7 @@ gsutil cat gs://govuk-knowledge-graph-content-store/mongo.tar.gz \
   | mongorestore -v --db=content_store --collection=content_items -
 
 # Query the content store into intermediate datasets
-gsutil cat gs://govuk-knowledge-graph-repository/src/mongodb/prepare-content-store.js \
-  | bash
+gsutil cat gs://govuk-knowledge-graph-repository/src/mongodb/prepare-content-store.js | mongo
 
 # Download from the content store and intermediate datasets, upload to storage
 gsutil cat gs://govuk-knowledge-graph-repository/src/mongodb/extract-from-mongodb.sh \
