8ac131e1d263ce99e7e1a85c8023a65bccf4488d
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-08-25 14:34:36 +0100

    Fix some neo4j run file mistakes

--- docker/neo4j/run.sh
+++ docker/neo4j/run.sh
@@ -6,7 +6,7 @@
 set -m
 
 # Start neo4j and put it in the background
-neo4j &
+neo4j start &
 
 # Wait for neo4j to start
 sleep 5
@@ -15,18 +15,18 @@ sleep 5
 
 # Download the files to the local Neo4j import directory, because Neo4j can't
 # import from a pipe.
-gsutil -m cp -rv gs://govuk-knowledge-graph-data-processed/content-store/\*
-/var/lib/neo4j/import/
+gsutil -m cp -rv \
+  gs://govuk-knowledge-graph-data-processed/content-store/\* \
+  /var/lib/neo4j/import
 
 # Decompress all those files (the semicolon is escaped for the shell, but might
 # not need to be escaped within a script).
 find /var/lib/neo4j/import -name "*.csv.gz" -exec gunzip {} \;
 
-# Obtain the latest state of the repository
-gsutil -m cp -r gs://govuk-knowledge-graph-repository/\* .
-
 # Query the content store into intermediate datasets
-cypher-shell -f src/neo4j/load-content-store-data.cypher
+gsutil cat \
+  gs://govuk-knowledge-graph-repository/src/neo4j/load-content-store-data.cypher \
+  | cypher-shell
 
 # Bring the background process back into the foreground and leave it there
 fg %1
