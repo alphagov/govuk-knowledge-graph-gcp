29ce0f2152056d85e872470acf932901a132ff30
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-09-01 14:35:02 +0100

    Add files that were omitted from #101
    Should have been in 40f7f5b29de865be17e9e397ccdd62624c99851d

--- src/mongodb/sh/expanded_links.sh
+++ src/mongodb/sh/expanded_links.sh
@@ -3,3 +3,11 @@ query_mongo \
   collection=expanded_links \
   fields=link_type,from_url,to_url \
 | upload file_name=expanded_links
+
+# expanded links by content_id
+query_mongo \
+  collection=expanded_links_content_ids \
+  fields=link_type,from_content_id,to_content_id | less
+
+\
+| upload file_name=expanded_links_content_ids
