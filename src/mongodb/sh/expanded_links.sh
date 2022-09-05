890b20d013bd9fa005517e6f6369e87d9a2d7254
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-09-05 12:06:01 +0100

    Remove dev code

--- src/mongodb/sh/expanded_links.sh
+++ src/mongodb/sh/expanded_links.sh
@@ -7,7 +7,5 @@ query_mongo \
 # expanded links by content_id
 query_mongo \
   collection=expanded_links_content_ids \
-  fields=link_type,from_content_id,to_content_id | less
-
-\
+  fields=link_type,from_content_id,to_content_id \
 | upload file_name=expanded_links_content_ids
