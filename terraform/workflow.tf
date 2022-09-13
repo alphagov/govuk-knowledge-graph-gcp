58a0e14dd8fde5e5aca7db234b49c3a22ccc5667
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-09-13 09:05:16 +0100

    Conditionally start MongoDB based on filename
    - Parse the PubSub message and log intermediate steps
    - Conditionally start a MongoDB instance
    - Pass the filepath to the instance

--- terraform/workflow.tf
+++ terraform/workflow.tf
@@ -21,16 +21,70 @@ resource "google_workflows_workflow" "mongodb" {
   # - [TBC] Creates an instance from the Neo4j template
   # In terraform you need to escape the $$ or it will cause errors.
 
-  - start_mongodb:
-      call: googleapis.compute.v1.instances.insert
+main:
+  params: [event]
+  steps:
+  - log_event:
+      call: sys.log
       args:
-          project: ${var.project_id}
-          zone: ${var.zone}
-          sourceInstanceTemplate: https://www.googleapis.com/compute/v1/projects/${var.project_id}/global/instanceTemplates/mongodb
-          body:
-              name: mongodb
+          text: $${event}
+          severity: INFO
+  - decode_pubsub_message:
+      assign:
+      - base64: $${base64.decode(event.data.data)}
+      - message_text: $${text.decode(base64)}
+  - log_message_text:
+      call: sys.log
+      args:
+          text: $${message_text}
+          severity: INFO
+  - parse_pubsub_message:
+      assign:
+      - message_json: $${json.decode(message_text)}
+  - log_message_json:
+      call: sys.log
+      args:
+          json: $${message_json}
+          severity: INFO
+  - extract_filepath:
+      assign:
+      - filepath: $${message_json.name}
+  - log_filepath:
+      call: sys.log
+      args:
+          text: $${filepath}
+          severity: INFO
+  - maybe_start_mongodb:
+      switch:
+        - condition: $${text.match_regex(filepath, "^mongo-api/\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}-content_store_production\\.gz$")}
+          steps:
+          - log_starting_instance:
+              call: sys.log
+              args:
+                  text: "Starting mongodb instance"
+                  severity: INFO
+          - start_mongodb:
+              call: googleapis.compute.v1.instances.insert
+              args:
+                  project: ${var.project_id}
+                  zone: ${var.zone}
+                  sourceInstanceTemplate: https://www.googleapis.com/compute/v1/projects/${var.project_id}/global/instanceTemplates/mongodb
+                  body:
+                      name: mongodb
+                      metadata:
+                          database_filepath: $${filepath}
+        - condition: true
+          steps:
+          - log_not_starting_instance:
+              call: sys.log
+              args:
+                  text: "Not starting mongodb instance"
+                  severity: INFO
 EOF
 }
+
+# An example of the decoded message:
+#
 
 resource "google_eventarc_trigger" "govuk_integration_database_backups" {
   name            = "mongodb"
