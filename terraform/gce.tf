eb196e7cb3776d124b0d2d092e9cca28647d8453
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-09-21 14:12:18 +0100

    Use smaller Neo4j disk

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -215,7 +215,7 @@ resource "google_compute_instance_template" "neo4j" {
   disk {
     boot         = true
     source_image = module.neo4j-container.source_image
-    disk_size_gb = 64
+    disk_size_gb = 40
   }
 
   metadata = {
