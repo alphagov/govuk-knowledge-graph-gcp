c001837ff43e0c67aab7de79fdef299662b8429a
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-10-04 15:29:38 +0100

    A config that makes the SSL certificate work

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -1,5 +1,73 @@
 # https://cloud.google.com/load-balancing/docs/https/ext-https-lb-simple
 
+# Bucket for a default HTML page that is open to the public
+resource "google_storage_bucket" "website" {
+  name                        = "${var.project_id}-website" # Must be globally unique
+  force_destroy               = false                              # terraform won't delete the bucket unless it is empty
+  location                    = var.location
+  storage_class               = "STANDARD" # https://cloud.google.com/storage/docs/storage-classes
+  uniform_bucket_level_access = true
+  versioning {
+    enabled = false
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "website" {
+  bucket      = google_storage_bucket.website.name
+  policy_data = data.google_iam_policy.bucket_website.policy_data
+}
+
+data "google_iam_policy" "bucket_website" {
+  binding {
+    role = "roles/storage.objectViewer"
+    members = [
+      "allUsers",
+    ]
+  }
+
+  binding {
+    role = "roles/storage.legacyBucketOwner"
+    members = [
+      "projectEditor:govuk-knowledge-graph",
+      "projectOwner:govuk-knowledge-graph",
+    ]
+  }
+
+  binding {
+    role = "roles/storage.legacyBucketReader"
+    members = [
+      "projectViewer:govuk-knowledge-graph",
+    ]
+  }
+
+  binding {
+    role = "roles/storage.legacyObjectOwner"
+    members = [
+      "projectEditor:govuk-knowledge-graph",
+      "projectOwner:govuk-knowledge-graph",
+    ]
+  }
+
+  binding {
+    role = "roles/storage.legacyObjectReader"
+    members = [
+      "projectViewer:govuk-knowledge-graph",
+    ]
+  }
+}
+
+resource "google_storage_bucket_object" "website_index" {
+  name         = "index.html"
+  content      = file("index.html")
+  content_type = "text/html"
+  bucket       = google_storage_bucket.website.name
+}
+
+resource "google_compute_backend_bucket" "website" {
+  name        = "website"
+  bucket_name = google_storage_bucket.website.name
+}
+
 # Static external IP address for Neo4j.  Global addresses don't work, because
 # they are only for load balancers, so it must be regional.
 resource "google_compute_global_address" "govgraph" {
@@ -16,7 +84,7 @@ resource "google_dns_managed_zone" "govgraph" {
   dnssec_config {
     kind          = "dns#managedZoneDnsSecConfig"
     non_existence = "nsec3"
-    state         = "off"
+    state         = "on"
     default_key_specs {
       algorithm  = "rsasha256"
       key_length = 2048
@@ -45,7 +113,6 @@ resource "google_dns_record_set" "govgraph" {
 # SSL Certificate
 resource "google_compute_managed_ssl_certificate" "govgraph" {
   name = "govgraph"
-
   managed {
     domains = ["govgraph.dev."]
   }
@@ -80,10 +147,30 @@ resource "google_compute_target_https_proxy" "govgraph" {
   ssl_certificates = [google_compute_managed_ssl_certificate.govgraph.id]
 }
 
+# resource "google_compute_target_http_proxy" "govgraph" {
+#   name             = "govgraph"
+#   url_map          = google_compute_url_map.govgraph.id
+# }
+
 resource "google_compute_url_map" "govgraph" {
   name            = "govgraph"
   description     = "URL map for govgraph.dev"
-  default_service = google_compute_backend_service.govgraph.id
+  default_service = google_compute_backend_bucket.website.id
+
+#   host_rule {
+#     hosts        = ["*"]
+#     path_matcher = "browser"
+#   }
+
+#   path_matcher {
+#     name            = "browser"
+#     default_service = google_compute_backend_bucket.website.id
+
+#     path_rule {
+#       paths   = ["/browser/*"]
+#       service = google_compute_backend_service.govgraph.id
+#     }
+#   }
 }
 
 resource "google_compute_backend_service" "govgraph" {
@@ -91,7 +178,16 @@ resource "google_compute_backend_service" "govgraph" {
   port_name     = "neo4j"
   health_checks = [google_compute_health_check.govgraph.id]
   backend {
+    balancing_mode               = "UTILIZATION"
+    capacity_scaler              = 1
     group = google_compute_instance_group.govgraph.id
+    max_connections              = 0
+    max_connections_per_endpoint = 0
+    max_connections_per_instance = 0
+    max_rate                     = 0
+    max_rate_per_endpoint        = 0
+    max_rate_per_instance        = 0
+    max_utilization              = 0
   }
 }
 
@@ -106,6 +202,8 @@ resource "google_compute_global_forwarding_rule" "govgraph" {
   name        = "govgraph"
   ip_address  = google_compute_global_address.govgraph.id
   target      = google_compute_target_https_proxy.govgraph.id
+  # target      = google_compute_target_http_proxy.govgraph.id
   ip_protocol = "TCP"
   port_range  = 443
+  # port_range  = 80
 }
