7a4c4c9194ea48ccfcb93ae99020e1d827f86b82
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2022-10-13 20:23:54 +0100

    Fix first_published_at and public_updated_at
    Fields that existed were sometimes null

--- src/mongodb/sh/first_published_at.sh
+++ src/mongodb/sh/first_published_at.sh
@@ -2,7 +2,7 @@ FILE_NAME=first_published_at
 
 query_mongo \
   fields=url,first_published_at \
-  query='{ "first_published_at": { "$exists": true } }' \
+  query='{ "first_published_at": { "$exists": true, "$ne": null } }' \
 | upload file_name=$FILE_NAME
 
 send_to_bigquery file_name=$FILE_NAME
--- src/mongodb/sh/public_updated_at.sh
+++ src/mongodb/sh/public_updated_at.sh
@@ -2,7 +2,7 @@ FILE_NAME=public_updated_at
 
 query_mongo \
   fields=url,public_updated_at \
-  query='{ "public_updated_at": { "$exists": true } }' \
+  query='{ "public_updated_at": { "$exists": true, "$ne": null } }' \
 | upload file_name=$FILE_NAME
 
 send_to_bigquery file_name=$FILE_NAME
