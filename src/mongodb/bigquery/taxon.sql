6e7aef7978ce95c013a59663c35d990b7a2f8a28
Max Froumentin <max.froumentin@digital.cabinet-office.gov.uk>
2023-01-17 14:15:47 +0000

    Use the correct URL for the taxon_levels join

--- src/mongodb/bigquery/taxon.sql
+++ src/mongodb/bigquery/taxon.sql
@@ -6,5 +6,5 @@ SELECT
   page.content_id,
   taxon_levels.level
 FROM content.taxon_levels
-INNER JOIN graph.page USING (url)
+INNER JOIN graph.page AS page ON taxon_levels.homepage_url = page.url
 ;
