ca98c2a6df06c5a79d4cbe6aa4aabf341b157e76
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2023-03-03 12:34:49 +0000

    Fix more glitches in graph.page creation

--- src/mongodb/bigquery/page.sql
+++ src/mongodb/bigquery/page.sql
@@ -21,7 +21,7 @@ SELECT
   c.text,
   CAST(NULL AS INT64) AS part_index,
   CAST(NULL AS STRING) AS slug,
-  page_views.number_of_views,
+  page_views.number_of_views AS page_views,
   pagerank.pagerank
 FROM content.url AS u
 LEFT JOIN content.document_type USING (url)
@@ -56,14 +56,14 @@ SELECT
     c.text AS text,
     parts.part_index AS part_index,
     parts.slug AS slug,
-    page_views.number_of_views,
-    pagerank.pagerank
+    page_views.number_of_views AS page_views,
+    pagerank.pagerank AS pagerank
   )
 FROM graph.page
 INNER JOIN content.parts ON page.url = parts.base_path
 LEFT JOIN content.content AS c ON c.url = parts.url
 LEFT JOIN content.page_views on (page_views.url = parts.url)
-LEFT JOIN content.pagerank on (pagerank.url = parts.url) (url)
+LEFT JOIN content.pagerank on (pagerank.url = parts.url)
 ;
 INSERT INTO graph.page SELECT * FROM graph.part;
 
