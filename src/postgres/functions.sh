c4601e1f932abb5ce463670ef8c78d08a9d1899f
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2023-03-08 17:58:05 +0000

    Export all publishing database tables to BigQuery
    This will make the data available without having to wait an hour for the
    database to restore, and without having to pay for an always-on postgres
    database.
    
    The `editions` table is too big as a CSV (19GB when compressed) for
    BigQuery to import, so DuckDB is used to convert it to the parquet
    format, which BigQuery will import at up sizes to 15TB.

--- src/postgres/functions.sh
+++ src/postgres/functions.sh
@@ -37,7 +37,7 @@ count_distinct () {
 # Extract datasets of nodes, attributes and edges from the PostgreSQL Publishing
 # API database.
 
-# Wrapper around psql.
+# Wrapper around psql to export the results of a query to CSV
 #
 # Usage:
 #
@@ -53,7 +53,7 @@ query_postgres () {
     --file="${file}"
 }
 
-# Wrapper around psql, for when emitting json
+# Wrapper around psql to export the results of a query to JSON
 #
 # Usage:
 #
@@ -67,6 +67,21 @@ query_postgres_json () {
 		--dbname=publishing_api_production \
 		--tuples-only \
     --file="${file}"
+}
+
+# Wrapper around psql to export a whole table to CSV
+#
+# Usage:
+#
+# dump_table \
+#   name=table_name
+dump_table () {
+  local name # reset in case they are defined globally
+  local "${@}"
+  psql \
+		--username=postgres \
+		--dbname=publishing_api_production \
+    --command="\copy ${name} TO STDOUT WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');"
 }
 
 # Wrapper around sed to replace single backslash with double backslashes,
@@ -200,4 +215,54 @@ query_bigquery () {
   bq query \
     --use_legacy_sql=false \
     < "${file_name}"
+}
+
+# Export a table, upload to storage and import into BigQuery
+#
+# Usage:
+# query_bigquery table_name=name_of_table_in_postgres
+export_to_bigquery () {
+  local table_name # reset in case they are defined globally
+  local "${@}"
+
+  file_name="table_${table_name}"
+  dataset_name="publishing"
+
+  # We have to use uncompressed CSV for the largest table, so we might as well
+  # use it for the others too.
+  # BigQuery can't deal with compressed CSVs larger than 4GB.  The largest table
+  # is 19GB when compressed.
+  # Alternatively we could use Parquet (via DuckDB) but BigQuery can't cope with
+  # values larger than 10MiB in any particular column, and the largest page has
+  # 11302027 bytes (>10MiB) in the `details` column.
+  # /hmrc-internal-manuals/customs-authorisation-and-approval/caa08030
+  # https://webarchive.nationalarchives.gov.uk/ukgwa/20211120003440/https://www.gov.uk/hmrc-internal-manuals/customs-authorisation-and-approval/caa08030
+  # The DuckDB max_line_size parameter defaults to 2 MiB, which would eliminate
+  # those rows before they break BigQuery, but DuckDB stops with an error
+  # instead of ignoring those lines.
+  # The command gcloud bq load has options for ignoring bad lines, but they
+  # don't work for parquet.
+  # Finally, BigQuery does in fact seem to allow values larger than 10MiB, as
+  # long as they come from uncompressed CSV files.  So that's what we use.
+  # What a palaver.  "Big" Query, huh.
+  dump_table \
+    name=$table_name \
+  | gcloud storage cp \
+    - \
+    "gs://${PROJECT_ID}-data-processed/publishing-api/${file_name}.csv" \
+    --gzip-in-flight-all
+
+  # Empty the table
+  bq query --use_legacy_sql=false "TRUNCATE TABLE ${dataset_name}.${table_name}"
+
+  # Use nosynchronous_mode to avoid waiting for BQ to complete one job before
+  # initiating another.
+  bq load \
+    --source_format="CSV" \
+    --allow_quoted_newlines \
+    --skip_leading_rows=1 \
+    --noreplace \
+    --nosynchronous_mode \
+    "${dataset_name}.${table_name}" \
+    "gs://${PROJECT_ID}-data-processed/publishing-api/${file_name}.csv"
 }
