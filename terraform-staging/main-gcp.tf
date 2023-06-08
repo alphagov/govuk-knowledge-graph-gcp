2a0ddef30d7b61ab4555e16525dd17c06d97f0f8
j-marvin-GDS <james.marvin@digitl.cabinet-office.gov.uk>
2023-06-08 15:09:58 +0100

    Removed AB permissions.
    Removed explicit mention of Anthony from files enabling permissions in dev, staging, prod.

--- terraform-staging/main-gcp.tf
+++ terraform-staging/main-gcp.tf
@@ -196,7 +196,6 @@ data "google_iam_policy" "project" {
     members = [
       "user:duncan.garmonsway@digital.cabinet-office.gov.uk",
       "user:james.marvin@digital.cabinet-office.gov.uk",
-      "user:anthony.borisade@digital.cabinet-office.gov.uk",
       "user:guilhem.forey@digital.cabinet-office.gov.uk",
       "user:somme.sakounthong@digital.cabinet-office.gov.uk",
     ]
