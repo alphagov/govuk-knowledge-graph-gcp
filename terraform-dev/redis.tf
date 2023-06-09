46649900604d10a554856d22bbedd8ea899eaeb5
Guilhem Forey <guilhem.forey@hotmail.fr>
2023-06-09 13:31:16 +0100

    TF: Added Redis Session Store

---
+++ terraform-dev/redis.tf
@@ -0,0 +1,7 @@
+resource "google_redis_instance" "session_store" {
+    name    =   "session-store"
+    tier    =   "STANDARD_HA"
+    memory_size_gb = 1
+    region = var.region
+    authorized_network = google_compute_network.cloudrun.name
+}
