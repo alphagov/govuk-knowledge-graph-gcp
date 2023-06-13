5e89c88ec952c43fb693bc32ad5df9fe3f4f4d86
Guilhem Forey <guilhem.forey@hotmail.fr>
2023-06-13 17:59:37 +0100

    Added iam binding for creating Redis instance

--- terraform-dev/redis.tf
+++ terraform-dev/redis.tf
@@ -1,7 +1,17 @@
+resource "google_project_iam_binding" "redis_service_agent_binding" {
+  project       = var.project_id
+  role          = "roles/redis.serviceAgent"
+  members       = ["serviceAccount:service-${var.project_number}@cloud-redis.iam.gserviceaccount.com"]
+}
+
+
 resource "google_redis_instance" "session_store" {
     name    =   "session-store"
     tier    =   "STANDARD_HA"
     memory_size_gb = 1
     region = var.region
-    # authorized_network = google_compute_network.cloudrun.name
+    authorized_network = google_compute_network.cloudrun.name
+
+    # Service account needs to be binded to this role before creating the instance
+    depends_on = [google_project_iam_binding.redis_service_agent_binding]
 }
