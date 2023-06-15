607bfcf145275a98e89c4ef2036530fb7294173d
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2023-06-15 15:38:10 +0100

    Ignore whitespace in blank terraform diffs
    Fix a bug in `diff-terraform` that causes it to exit with an
    error code even when the diff is blank, because blank diffs have
    whitespace.

--- diff-terraform.sh
+++ diff-terraform.sh
@@ -14,4 +14,5 @@ echo "$diffs"
 
 # If there are any differences then exit with an error.
 # https://stackoverflow.com/a/35165216/937932
-if [[ $(echo $diffs | head -c1 | wc -c) -ne 0 ]]; then exit 1; fi
+# Leading whitespace of an empty diff is trimmed with the tr command.
+if [[ $(echo $diffs | tr -d "[:space:]" | head -c1 | wc -c) -ne 0 ]]; then exit 1; else exit 0; fi
