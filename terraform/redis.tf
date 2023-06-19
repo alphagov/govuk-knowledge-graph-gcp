277b1f03b67e9160245e0fd497ea272f3514074d
Guilhem Forey <guilhem.forey@digital.cabinet-office.gov.uk>
2023-06-19 13:29:43 +0100

    Apply formatting

--- terraform/redis.tf
+++ terraform/redis.tf
@@ -1,10 +1,10 @@
 resource "google_redis_instance" "session_store" {
-    name    =   "session-store"
-    tier    =   "STANDARD_HA"
-    memory_size_gb = 1
-    region = var.region
-    authorized_network = google_compute_network.cloudrun.name
-    
-    # Enable / Disable instance
-    count = var.enable_redis_session_store_instance ? 1 : 0
+  name               = "session-store"
+  tier               = "STANDARD_HA"
+  memory_size_gb     = 1
+  region             = var.region
+  authorized_network = google_compute_network.cloudrun.name
+
+  # Enable / Disable instance
+  count = var.enable_redis_session_store_instance ? 1 : 0
 }
