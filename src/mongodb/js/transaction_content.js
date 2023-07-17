9b4846bf2e7e75a94eb88f2a705a16e63e4e84a4
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2023-07-17 12:13:48 +0100

    Extract content of licence and local_transaction
    Extract the content of two schemas that are similar to the transaction
    schema.

--- src/mongodb/js/transaction_content.js
+++ src/mongodb/js/transaction_content.js
@@ -2,16 +2,21 @@
 // https://github.com/alphagov/govuk-content-schemas/blob/main/dist/formats/transaction/frontend/schema.json
 db.content_items.aggregate([
   { $match: { "schema_name": { $in: [
+    "licence",
+    "local_transaction",
     "transaction"
   ] } } },
   { $project: {
     "url": true,
-    "details.introductory_paragraph": true,
-    "details.start_button_text": true,
-    "details.will_continue_on": true,
-    "details.more_information": true,
-    "details.what_you_need_to_know": true,
-    "details.other_ways_to_apply": true,
+    "details.introductory_paragraph": true, // transaction
+    "details.introduction": true, // local_transaction
+    "details.licence_overview": true, // licence
+    "details.start_button_text": true, // transaction
+    "details.will_continue_on": true, // transaction
+    "details.more_information": true, // transaction, local_transaction
+    "details.what_you_need_to_know": true, // transaction
+    "details.need_to_know": true, // local_transaction
+    "details.other_ways_to_apply": true, // transaction
   } },
   // Omit govspeak content
   { $redact: {
@@ -27,12 +32,15 @@ db.content_items.aggregate([
   { $project: {
     url: true,
     content: { $concatArrays: [
-      { $ifNull: [ "$details.introductory_paragraph.content", [] ] },
-      [ { $ifNull: [ "$details.start_button_text", "" ] } ],
-      { $ifNull: [ "$details.will_continue_on.content", [] ] },
-      { $ifNull: [ "$details.more_information.content", [] ] },
-      { $ifNull: [ "$details.what_you_need_to_know.content", [] ] },
-      { $ifNull: [ "$details.other_ways_to_apply.content", [] ] },
+      { $ifNull: [ "$details.introductory_paragraph.content", [] ] }, // transaction
+      { $ifNull: [ "$details.introduction.content", [] ] }, // local_transaction
+      { $ifNull: [ "$details.licence_overview.content", [] ] }, // licence
+      [ { $ifNull: [ "$details.start_button_text", "" ] } ], // transaction
+      { $ifNull: [ "$details.will_continue_on.content", [] ] }, // transaction
+      { $ifNull: [ "$details.more_information.content", [] ] }, // transaction, local_transaction
+      { $ifNull: [ "$details.what_you_need_to_know.content", [] ] }, // transaction
+      { $ifNull: [ "$details.need_to_know.content", [] ] }, // local_transaction
+      { $ifNull: [ "$details.other_ways_to_apply.content", [] ] }, // transaction
     ] }
   } },
   // Concatenate all the strings, separated by newlines
