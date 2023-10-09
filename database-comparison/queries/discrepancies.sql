ba389dd87cc3a784b9dbdceff65b26013b63e233
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2023-10-09 22:35:33 +0100

    Further investigate the content store's contents
    Things that don't completely explain what is included in the content
    store:
    
    - state = 'published', because unpublished editions may remain as
      redirects or withdrawals
    - phase apparently has nothing to do with anything
    - content_store because lots of redirects seem to have been implemented
      as content_store not being live and state being superseded instead of
      unpublished
    - edition.redirects is only one way of recording a redirect
    - unpublishings.redirects is the other way of recording a redirect
    - placeholder because a few aren't explained by other flags

--- database-comparison/queries/discrepancies.sql
+++ database-comparison/queries/discrepancies.sql
@@ -27,35 +27,32 @@ select
   state,
   phase,
   content_store,
-  jsonb_array_length(redirects) AS redirects,
-  schema_name like '%placeholder%' AS placeholder
+  jsonb_array_length(editions_latest.redirects) AS redirects,
+  schema_name like '%placeholder%' AS placeholder,
+  unpublishings.type AS unpublishing_type
 from editions_latest
 inner join register using (base_path)
+left join unpublishings on editions_latest.id = unpublishings.edition_id
 where content_mongo is null
 )
 select *, count(*) as n
 from features
-group by state, phase, content_store, redirects, placeholder
-order by state, phase, content_store, redirects, placeholder
+group by state, phase, content_store, redirects, placeholder, unpublishing_type
+order by state, phase, content_store, redirects, placeholder, unpublishing_type
 ;
-
---     state    | phase | content_store | redirects | placeholder |   n
--- -------------+-------+---------------+-----------+-------------+-------
---  draft       | alpha | draft         |         0 | f           |    11
---  draft       | beta  | draft         |         0 | f           |    10
---  draft       | live  | draft         |         0 | f           | 21161
---  draft       | live  | draft         |         0 | t           |  1023
---  draft       | live  | draft         |         1 | f           | 42943
---  draft       | live  | draft         |         2 | f           |     1
---  published   | live  | live          |         0 | f           |   233 truly published, content store is a bit behind
---  published   | live  | live          |         0 | t           |   683
---  published   | live  | live          |         1 | f           |     4
---  superseded  | live  |               |         0 | t           |    23
---  unpublished | alpha | live          |         0 | f           |     1
---  unpublished | live  | live          |         0 | f           |   286
---  unpublished | live  | live          |         0 | t           |   100
---  unpublished | live  | live          |         1 | f           |     4
---  unpublished | live  |               |         0 | f           |    22
+--     state    | phase | content_store | redirects | placeholder | unpublishing_type |  n
+-- -------------+-------+---------------+-----------+-------------+-------------------+------
+--  published   | live  | live          |         0 | f           |                   |  235 truly published, content store is a bit behind
+--  published   | live  | live          |         0 | t           |                   | 1218
+--  published   | live  | live          |         1 | f           |                   |    4
+--  superseded  | live  |               |         0 | f           |                   |   30
+--  superseded  | live  |               |         0 | t           |                   |  655
+--  unpublished | alpha | live          |         0 | f           | vanish            |    1
+--  unpublished | live  | live          |         0 | f           | vanish            |  293
+--  unpublished | live  | live          |         0 | t           | vanish            |   99
+--  unpublished | live  | live          |         0 | t           | withdrawal        |    1
+--  unpublished | live  | live          |         1 | f           | gone              |    1
+--  unpublished | live  | live          |         1 | f           | vanish            |    3
 
 -- What "live" pages are in the publishing database but not the mongo content
 -- store?
@@ -72,3 +69,90 @@ where
   and not schema_name like '%placeholder%'
 order by updated_at desc
 ;
+
+-- What kind of pages are in the mongo content store?
+with features AS (
+select
+  state,
+  phase,
+  content_store,
+  NOT jsonb_array_length(editions_latest.redirects) = 0 AS redirects,
+  schema_name like '%placeholder%' AS placeholder,
+  unpublishings.type AS unpublishing_type
+from editions_latest
+inner join register using (base_path)
+left join unpublishings on editions_latest.id = unpublishings.edition_id
+where content_mongo
+)
+select *, count(*) as n
+from features
+group by state, phase, content_store, redirects, placeholder, unpublishing_type
+order by state, phase, content_store, redirects, placeholder, unpublishing_type
+;
+--     state    | phase | content_store | redirects | placeholder | unpublishing_type |   n
+-- -------------+-------+---------------+-----------+-------------+-------------------+--------
+--  published   | alpha | live          | f         | f           |                   |    144
+--  published   | beta  | live          | f         | f           |                   |  83715
+--  published   | beta  | live          | t         | f           |                   |    673
+--  published   | live  | live          | f         | f           |                   | 585587
+--  published   | live  | live          | f         | t           |                   |     46
+--  published   | live  | live          | t         | f           |                   |  86785
+--  superseded  | live  |               | f         | f           | gone              |     91
+--  superseded  | live  |               | f         | f           | redirect          |   6363
+--  superseded  | live  |               | f         | f           | substitute        |      3
+--  superseded  | live  |               | f         | f           |                   |    163
+--  superseded  | live  |               | f         | t           | gone              |      1
+--  superseded  | live  |               | f         | t           | redirect          |    743
+--  superseded  | live  |               | f         | t           |                   |      7
+--  unpublished | alpha | live          | f         | f           | gone              |      4
+--  unpublished | alpha | live          | f         | f           | redirect          |    602
+--  unpublished | beta  | live          | f         | f           | redirect          |    108
+--  unpublished | live  | live          | f         | f           | gone              |   4896
+--  unpublished | live  | live          | f         | f           | redirect          |  68578
+--  unpublished | live  | live          | f         | f           | withdrawal        |  52801
+--  unpublished | live  | live          | f         | t           | gone              |    344
+--  unpublished | live  | live          | f         | t           | redirect          |    821
+--  unpublished | live  | live          | t         | f           | gone              |     11
+--  unpublished | live  | live          | t         | f           | redirect          |    756
+--  unpublished | live  |               | f         | f           | gone              |     13
+--  unpublished | live  |               | f         | f           | redirect          |     95
+--  unpublished | live  |               | f         | t           | gone              |      1
+--  unpublished | live  |               | t         | f           | substitute        |    211
+
+-- What pages are only in the mongo content store, not in publishing?
+select base_path
+from register
+left join editions_latest using(base_path)
+where editions_latest.base_path is null
+;
+
+-- Why are superseded pages in the mongo content store?
+with features AS (
+select
+  base_path,
+  state,
+  phase,
+  content_store,
+  jsonb_array_length(redirects) AS redirects,
+  schema_name like '%placeholder%' AS placeholder
+from editions_latest
+inner join register using (base_path)
+)
+select *
+from features
+where state = 'unpublished'
+and phase = 'live'
+and content_store = 'live'
+and redirects = 0
+and not placeholder
+;
+
+select * from editions_latest where base_path = '/guidance/poultry-registration';
+select * from editions where base_path = '/guidance/poultry-registration' order by updated_at;
+select * from editions where base_path = '/government/publications/poultry-including-game-birds-registration-rules-and-forms' order by updated_at;
+
+-- Why are pages that in real life are redirected, in the publishing database as
+-- not being redirected?
+-- Because redirects can be expressed in the 'unpublishings' table instead.
+select * from editions where base_path = '/growth-loans-london' order by updated_at;
+select * from editions_latest where base_path = '/growth-loans-london';
