8104ecb5a1592b392decec6905444c64aeade7bb
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2023-12-13 16:39:34 +0000

    feat: BigQuery UDF to find phone numbers in text
    This uses a Google library called libphonenumber to extract phone
    numbers from plain text, and standardise them so that they are easier to
    match.  For example "01234 567 890" might be standardised to
    "+441234567890".  A user query in GovSearch could also be standardised,
    so that comparisons are always between standard forms. Otherwise a user
    might search for "01234 567890", which wouldn't match "01234 567 890".
    
    Google's own javascript implementation is difficult to deploy, so we use
    a different distribution that Google themselves suggest.  This is a
    single javascript file that is stored in a bucket.  The "max" version is
    used because GOV.UK has numbers in forms that aren't supported by the
    other versions.
    
    One UDF (user-defined-function) fetches the library from the bucket,
    imports it into a short javascript to process some text, and returns the
    results as JSON.
    
    A second UDF wraps the first one, and parses the JSON into a STRUCT,
    which is easier to work with.  This second function is necessary because
    Javascript UDFs can't return structs themselves.

---
+++ terraform-dev/bigquery-functions.tf
@@ -0,0 +1,86 @@
+# A dataset of user-defined functions and remote functions
+
+resource "google_bigquery_dataset" "functions" {
+  dataset_id            = "functions"
+  friendly_name         = "functions"
+  description           = "User-defined functions and remote functions"
+  location              = "europe-west2"
+  max_time_travel_hours = "48"
+}
+
+data "google_iam_policy" "bigquery_dataset_functions" {
+  binding {
+    role = "roles/bigquery.dataEditor"
+    members = [
+      "projectWriters",
+    ]
+  }
+  binding {
+    role = "roles/bigquery.dataOwner"
+    members = [
+      "projectOwners",
+    ]
+  }
+  binding {
+    role = "roles/bigquery.dataViewer"
+    members = [
+      "projectReaders",
+      "group:govsearch-data-viewers@digital.cabinet-office.gov.uk",
+    ]
+  }
+}
+
+resource "google_bigquery_dataset_iam_policy" "functions" {
+  dataset_id  = google_bigquery_dataset.functions.dataset_id
+  policy_data = data.google_iam_policy.bigquery_dataset_functions.policy_data
+}
+
+resource "google_bigquery_routine" "libphonenumber_find_phone_numbers_in_text" {
+  dataset_id   = google_bigquery_dataset.functions.dataset_id
+  routine_id   = "libphonenumber_find_phone_numbers_in_text"
+  routine_type = "SCALAR_FUNCTION"
+  language     = "JAVASCRIPT"
+  definition_body = templatefile(
+    "bigquery/libphonenumber-find-phone-numbers-in-text.js",
+    {
+      project_id = var.project_id
+    }
+  )
+  imported_libraries = [
+    // From https://github.com/catamphetamine/libphonenumber-js
+    "gs://govuk-knowledge-graph-dev-lib/libphonenumber-max.js",
+  ]
+  return_type = jsonencode(
+    {
+      typeKind = "JSON"
+    }
+  )
+
+  arguments {
+    data_type = jsonencode(
+      {
+        typeKind = "STRING"
+      }
+    )
+    name = "text"
+  }
+}
+
+resource "google_bigquery_routine" "extract_phone_numbers" {
+  dataset_id   = google_bigquery_dataset.functions.dataset_id
+  routine_id   = "extract_phone_numbers"
+  routine_type = "SCALAR_FUNCTION"
+  language     = "SQL"
+  definition_body = templatefile(
+    "bigquery/extract-phone-numbers.sql",
+    { project_id = var.project_id }
+  )
+  arguments {
+    data_type = jsonencode(
+      {
+        typeKind = "STRING"
+      }
+    )
+    name = "text"
+  }
+}
