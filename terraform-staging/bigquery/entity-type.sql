8c72e2c275922fc69fa34d30502a7227d1421de9
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-02-09 00:53:20 +0000

    feat: remove unused tables in the 'search' dataset
    Once the GovSearch infobox is removed by
    https://github.com/alphagov/govuk-knowledge-graph-search/pull/230 (if
    that happens), several tables in the `search` dataset can be removed.
    
    Ones that must remain are `page` and `taxon`. The `taxon` table could
    possibly be refactored.  New `organisation` and `locale` tables could be
    created to populate drop-down menus, which are currently populated from
    tables in other datasets.

--- terraform-staging/bigquery/entity-type.sql
+++
@@ -1,7 +0,0 @@
--- Table of things on GOV.UK, and the type of thing that they are
-TRUNCATE TABLE search.entityTypes;
-INSERT INTO search.entityTypes
-SELECT DISTINCT
-  type
-FROM `cpto-content-metadata.named_entities.named_entities_counts`
-;
