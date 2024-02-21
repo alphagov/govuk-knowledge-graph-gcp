0d8cae0e6ae6cb060c519fec71a294fc83c809a8
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-02-21 12:56:40 +0000

    refactor: add an intermediate step before public
    New, current, online editions aren't just appended to a table. They are
    also processed further, to extract the content. That means that it is
    useful to keep an intermediate step between scrubbing them of non-public
    data, and appending them to the main table.  That way, further
    processing can use the small intermediate step, rather than
    re-processing the whole main table.

--- terraform/bigquery/publishing-api-editions-current.sql
+++ terraform/bigquery/publishing-api-editions-current.sql
@@ -63,21 +63,49 @@ INSERT INTO private.publishing_api_editions_new_current
   QUALIFY ROW_NUMBER() OVER (PARTITION BY content_id, locale ORDER BY updated_at DESC) = 1
 ;
 
--- Delete rows from the editions_current table where a newer edition of the same
--- document is now available.
-MERGE INTO
-public.publishing_api_editions_current AS target
-USING private.publishing_api_editions_new_current AS source
-ON source.content_id = target.content_id AND source.locale = target.locale
-WHEN matched THEN DELETE
+-- Derive from the new editions a table of the latest edition per document, and
+-- a flag indicating whether it has a presence online (whether a redirect,
+-- or embedded in other pages, or a page in its own right).
+TRUNCATE TABLE private.publishing_api_editions_new_current;
+INSERT INTO private.publishing_api_editions_new_current
+  SELECT
+    documents.content_id,
+    documents.locale,
+    publishing_api_editions_new.*,
+    unpublishings.type AS unpublishing_type,
+    -- TODO: derive other values here
+    (
+      coalesce(content_store = 'live', false) -- Includes items that are only embedded in others.
+      AND state <> 'superseded' -- Exclude this rare and illogical case
+      AND coalesce(unpublishings.type <> 'vanish', true)
+      AND (
+        coalesce(left(schema_name, 11) <> 'placeholder', true)
+        OR (
+          -- schema_name must be checked again because short-circuit evaluation
+          -- isn't available in this clause.
+          coalesce(left(schema_name, 11) = 'placeholder', false)
+          AND coalesce(unpublishings.type IN ('gone', 'redirect'), false)
+        )
+      )
+    ) AS is_online
+  FROM private.publishing_api_editions_new
+  INNER JOIN publishing_api.documents ON documents.id = publishing_api_editions_new.document_id
+  LEFT JOIN publishing_api.unpublishings ON unpublishings.edition_id = publishing_api_editions_new.id
+  WHERE state <> 'draft'
+  QUALIFY ROW_NUMBER() OVER (PARTITION BY content_id, locale ORDER BY updated_at DESC) = 1
 ;
 
--- Insert new editions into the editions_current table, if they are also
--- 'online', which means that they are publicly available via the website or the
--- Content API. Scrub certain columns of editions that are redirected or
+-- Insert new editions into the public.editions_new_current table, if they are
+-- also 'online', which means that they are publicly available via the website
+-- or the Content API. Scrub certain columns of editions that are redirected or
 -- 'gone', and omit columns that aren't in the Content API at all.
 -- https://github.com/alphagov/publishing-api/tree/d041ae94a48fec9bd623bbb36ae6e87820ea0b06/app/presenters
-INSERT INTO public.publishing_api_editions_current
+--
+-- These could go straigt into public.publishing_api_editions_current, but it's
+-- more efficient to put them here, so that we can do downstream processing of
+-- only the new editions, without querying all the existing editions.
+TRUNCATE TABLE public.publishing_api_editions_new_current;
+INSERT INTO public.publishing_api_editions_new_current
 SELECT *
     EXCEPT (
       created_at,
@@ -94,17 +122,33 @@ SELECT *
       is_online
     )
     REPLACE (
-      IF (unpublishing_type IN ('redirect', 'gone'), unpublishing_type, document_type) AS document_type,
-      IF (unpublishing_type IN ('redirect', 'gone'), unpublishing_type, schema_name) AS schema_name,
-      IF (unpublishing_type IN ('redirect', 'gone'), NULL, title) AS title,
-      IF (unpublishing_type IN ('redirect', 'gone'), NULL, rendering_app) AS rendering_app,
-      IF (unpublishing_type IN ('redirect', 'gone'), NULL, analytics_identifier) AS analytics_identifier,
-      IF (unpublishing_type IN ('redirect', 'gone'), NULL, first_published_at) AS first_published_at,
-      IF (unpublishing_type IN ('redirect', 'gone'), NULL, description) AS description,
-      IF (unpublishing_type IN ('redirect', 'gone'), NULL, details) AS details
+      IF(unpublishing_type IN ('redirect', 'gone'), unpublishing_type, document_type) AS document_type,
+      IF(unpublishing_type IN ('redirect', 'gone'), unpublishing_type, schema_name) AS schema_name,
+      IF(unpublishing_type IN ('redirect', 'gone'), NULL, title) AS title,
+      IF(unpublishing_type IN ('redirect', 'gone'), NULL, rendering_app) AS rendering_app,
+      IF(unpublishing_type IN ('redirect', 'gone'), NULL, analytics_identifier) AS analytics_identifier,
+      IF(unpublishing_type IN ('redirect', 'gone'), NULL, first_published_at) AS first_published_at,
+      IF(unpublishing_type IN ('redirect', 'gone'), NULL, description) AS description,
+      IF(unpublishing_type IN ('redirect', 'gone'), NULL, details) AS details
     )
 FROM private.publishing_api_editions_new_current
 WHERE is_online
+;
+
+-- Delete rows from the editions_current table where a newer edition of the same
+-- document is now available.  The newer edition might be private, so use the
+-- private editions as the source of the merge.
+MERGE INTO
+public.publishing_api_editions_current AS target
+USING private.publishing_api_editions_new_current AS source
+ON source.content_id = target.content_id AND source.locale = target.locale
+WHEN matched THEN DELETE
+;
+
+-- Insert new, public editions into the
+-- public.publishing_api_editions_new_current table.
+INSERT INTO public.publishing_api_editions_current
+SELECT * FROM public.publishing_api_editions_new_current
 ;
 
 END
