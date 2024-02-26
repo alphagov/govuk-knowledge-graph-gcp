11caa73140596b64ec291cd0f597ce996bbb3295
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2024-02-26 16:23:48 +0000

    chore: format terraform

--- terraform/parse-html.tf
+++ terraform/parse-html.tf
@@ -10,7 +10,7 @@ resource "google_cloud_run_v2_service" "parse_html" {
       image = "europe-west2-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.docker.repository_id}/parse-html:latest"
       resources {
         limits = {
-          cpu    = "1000m" # If we put "1" or nothing, terraform reapplies it.
+          cpu    = "1000m"  # If we put "1" or nothing, terraform reapplies it.
           memory = "2048Mi" # By experiment, necessary and sufficient.
         }
       }
