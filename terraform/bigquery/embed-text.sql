a955657855bc7932b33cd45bdf531e82563e5371
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2025-01-28 10:04:45 +0000

    feat!: Remove the embed-text function
    It isn't being used, it's troublesome to maintain, and nowadays can be
    done better by Vertex AI.

--- terraform/bigquery/embed-text.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION `${project_id}.functions.embed_text`(text STRING)
-RETURNS JSON
-REMOTE WITH CONNECTION `${project_id}.${region}.embed-text`
-OPTIONS (
-  endpoint = "${uri}",
-  max_batching_rows=1
-)
