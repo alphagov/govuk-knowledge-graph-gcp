528068c5afe36458b51d20a4a485ada68124e3c1
Duncan Garmonsway <duncan.garmonsway@digital.cabinet-office.gov.uk>
2025-04-01 09:58:30 +0100

    draft: add smart survey

--- terraform-staging/bigquery-support-api.tf
+++ terraform-staging/bigquery-support-api.tf
@@ -1,20 +1,19 @@
 # A dataset of tables from the Support API postgres database
 
-resource "google_bigquery_dataset" "support_api" {
-  dataset_id            = "support_api"
-  friendly_name         = "Support API"
-  description           = "Data from the GOV.UK Support API database"
+resource "google_bigquery_dataset" "smart_survey" {
+  dataset_id            = "smart_survey"
+  friendly_name         = "Smart Survey"
+  description           = "Data from the Smart Survey API"
   location              = "europe-west2"
   max_time_travel_hours = "48"
 }
 
-data "google_iam_policy" "bigquery_dataset_support_api" {
+data "google_iam_policy" "bigquery_dataset_smart_survey" {
   binding {
     role = "roles/bigquery.dataEditor"
     members = [
       "projectWriters",
-      google_service_account.bigquery_scheduled_queries.member,
-      google_service_account.gce_support_api.member,
+      google_service_account.workflow_smart_survey.member,
     ]
   }
   binding {
@@ -29,28 +28,20 @@ data "google_iam_policy" "bigquery_dataset_support_api" {
       [
         "projectReaders",
       ],
-      var.bigquery_support_api_data_viewer_members,
+      var.bigquery_smart_survey_data_viewer_members,
     )
   }
 }
 
-resource "google_bigquery_dataset_iam_policy" "support_api" {
-  dataset_id  = google_bigquery_dataset.support_api.dataset_id
-  policy_data = data.google_iam_policy.bigquery_dataset_support_api.policy_data
+resource "google_bigquery_dataset_iam_policy" "smart_survey" {
+  dataset_id  = google_bigquery_dataset.smart_survey.dataset_id
+  policy_data = data.google_iam_policy.bigquery_dataset_smart_survey.policy_data
 }
 
-resource "google_bigquery_table" "support_api_anonymous_contacts" {
-  dataset_id    = google_bigquery_dataset.support_api.dataset_id
-  table_id      = "anonymous_contacts"
-  friendly_name = "Anonymous contacts"
-  description   = "Contact tickets (anonymous, long-form), problem reports (collected at the bottom of a page), service feedback (rating out of 5, and what could be improved)"
-  schema        = file("schemas/support-api/anonymous-contacts.json")
-}
-
-resource "google_bigquery_table" "support_api_archived_service_feedbacks" {
-  dataset_id    = google_bigquery_dataset.support_api.dataset_id
-  table_id      = "archived_service_feedbacks"
-  friendly_name = "Archived service feedback"
-  description   = "Service feedback (rating out of 5, and what could be improved)"
-  schema        = file("schemas/support-api/archived-service-feedbacks.json")
+resource "google_bigquery_table" "smart_survey_responses" {
+  dataset_id    = google_bigquery_dataset.smart_survey.dataset_id
+  table_id      = "responses"
+  friendly_name = "Survey responses"
+  description   = "One row per survey response"
+  schema        = file("schemas/smart-survey/responses.json")
 }
