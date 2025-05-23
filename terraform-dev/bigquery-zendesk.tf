1289bd73163a19b1227e3461b8282f94e48dd163
zilnhoj <john.byrne@digital.cabinet-office.gov.uk>
2025-05-23 15:12:52 +0100

    feat: add zendesk tickets
    Cribbing from #782

---
+++ terraform-dev/bigquery-zendesk.tf
@@ -0,0 +1,54 @@
+# A dataset of data from the Zenesk API
+
+resource "google_bigquery_dataset" "zendesk" {
+  dataset_id            = "zendesk"
+  friendly_name         = "Zendesk"
+  description           = "Data from the Zendesk API"
+  location              = "europe-west2"
+  max_time_travel_hours = "48"
+}
+
+data "google_iam_policy" "bigquery_dataset_zendesk" {
+  binding {
+    role = "roles/bigquery.dataEditor"
+    members = [
+      "projectWriters",
+      google_service_account.workflow_zendesk.member,
+    ]
+  }
+  binding {
+    role = "roles/bigquery.dataOwner"
+    members = [
+      "projectOwners",
+    ]
+  }
+  binding {
+    role = "roles/bigquery.dataViewer"
+    members = concat(
+      [
+        "projectReaders",
+      ],
+      var.bigquery_zendesk_data_viewer_members,
+    )
+  }
+}
+
+resource "google_bigquery_dataset_iam_policy" "zendesk" {
+  dataset_id  = google_bigquery_dataset.zendesk.dataset_id
+  policy_data = data.google_iam_policy.bigquery_dataset_zendesk.policy_data
+}
+/*
+resource "google_bigquery_table" "zendesk_responses" {
+  dataset_id               = google_bigquery_dataset.zendesk.dataset_id
+  table_id                 = "responses"
+  friendly_name            = "Zendesk responses"
+  description              = "Zendesk responses from the Zendesk API, fetched by the zendesk workflow. One row per zendesk response."
+  schema                   = file("schemas/zendesk/responses.json")
+  require_partition_filter = true
+  time_partitioning {
+    expiration_ms = 1000 * 60 * 60 * 24 * 365
+    field         = "date_started"
+    type          = "DAY"
+  }
+}
+*/
