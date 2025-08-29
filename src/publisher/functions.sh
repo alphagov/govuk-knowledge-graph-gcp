ce0a5312556e86e2396824e21a53778c3363f4f0
JonathanHallam <jonathan.hallam@digital.cabinet-office.gov.uk>
2025-08-29 15:39:36 +0100

    wip

--- src/publisher/functions.sh
+++ src/publisher/functions.sh
@@ -1,40 +1,79 @@
 #! /bin/bash
-set -e
 
-# Export a table from an SQL dump file, upload to storage and import into BigQuery
-export_query_to_bigquery () {
-  local mongo_database="$1"
-  local gcp_project_id="$2"
-  local bq_dataset="$3"
-  local bq_table="$4"
-  local fields="$5"
+# Export a table from a pg_dump file, upload to storage and import into BigQuery
+#
+# Usage:
+# query_bigquery backup_name=name_of_db_backup_file table_name=name_of_table_in_postgres
+export_to_bigquery () {
+  # reset variables in case they are defined globally
+  local backup_name
+  local table_name
+  local "${@}"
 
-  local query="${bq_table}.js"
-  local collection="${bq_table}_output"
-  local object="gs://${gcp_project_id}-data-processed/publisher/${bq_table}.csv.gz"
-  local table="${bq_dataset}.${bq_table}"
+  # Export data to the SSD, which is mapped to /data
+  tsv_name="/data/table_${table_name}"
 
-  # Create a dataset in mongodb of relevant metadata about relevant editions of
-  # documents.
-  mongosh "${mongo_database}" "${query}"
+  schema_name="schema_${table_name}"
+  dataset_name="publisher"
 
-  # Export that dataset
-  # Upload it to a cloud bucket
-  mongoexport \
-    --quiet \
-    --db="${mongo_database}" \
-    --type=csv \
-    --collection="${collection}" \
-    --fields="${fields}" \
-    | gcloud storage cp - "${object}" --quiet --gzip-in-flight-all
+  # We have to use uncompressed CSV for the largest table, so we might as well
+  # use it for the others too.
+  # BigQuery can't deal with compressed CSVs larger than 4GB.  The largest table
+  # is 19GB when compressed.
+  # Alternatively we could use Parquet (via DuckDB) but BigQuery can't cope with
+  # values larger than 10MiB in any particular column, and the largest page has
+  # 11302027 bytes (>10MiB) in the `details` column.
+  # /hmrc-internal-manuals/customs-authorisation-and-approval/caa08030
+  # https://webarchive.nationalarchives.gov.uk/ukgwa/20211120003440/https://www.gov.uk/hmrc-internal-manuals/customs-authorisation-and-approval/caa08030
+  # The DuckDB max_line_size parameter defaults to 2 MiB, which would eliminate
+  # those rows before they break BigQuery, but DuckDB stops with an error
+  # instead of ignoring those lines.
+  # The command gcloud bq load has options for ignoring bad lines, but they
+  # don't work for parquet.
+  # Finally, BigQuery does in fact seem to allow values larger than 10MiB, as
+  # long as they come from uncompressed CSV files.  So that's what we use.
+  # What a palaver.  "Big" Query, huh.
 
-  # Upload the dataset from the cloud bucket to a BigQuery table
-  bq --project_id "${gcp_project_id}" load \
-    --quiet=true \
-    --replace \
+  # Dump the table
+  #
+  # pg_restore emits SQL statements, which can be trimmed to resemble
+  # tab-separated values. Lines up to and including a COPY
+  # statement are discarded. Following lines are retained until a line that has
+  # only a backslash and a full stop.  That line and following lines are
+  # discarded. Lines that are retained are modified to unescape escaped
+  # backslashes, i.e. replace \\ with \.
+  pg_restore \
+    -U postgres \
+    --no-owner \
+    --data-only \
+    --file=- \
+    --table=$table_name \
+    $backup_name \
+    | sed -e '1,/^COPY/d' \
+    | sed -e 's/\\\\/\\/g' \
+    | sed -e '/^\\\./Q' \
+    > $tsv_name
+
+  # Download the existing schema
+  bq show \
+    --schema=true \
+    --format=json \
+    "${dataset_name}.${table_name}" \
+    > $schema_name
+
+  # Load data into the the table, using the "write disposition", which is
+  # equivalent to WRITE_TRUNCATE in SQL. It empties the table and wipes its
+  # schema, before inserting new rows. This is done within a transaction. We
+  # preserve the schema by downloading it first with `bq show`, and then using
+  # it as an argument to `bq load`.
+  bq load \
     --source_format="CSV" \
-    --allow_quoted_newlines \
+    --field_delimiter="\t" \
+    --null_marker="\\N" \
+    --quote="" \
     --skip_leading_rows=1 \
-    "${table}" \
-    "${object}"
+    --replace=true \
+    --schema="${schema_name}" \
+    "${dataset_name}.${table_name}" \
+    "${tsv_name}"
 }
