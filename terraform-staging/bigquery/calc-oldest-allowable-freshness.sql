f234e1c2b1071cc8d4f17e4c36d9136a2ce2fe3c
Aaron Fowles <aaron.fowles@digital.cabinet-office.gov.uk>
2025-12-22 12:57:00 +0000

    IA-2137: increase data freshness tolerance to two hours to account for daylight savings time changes

--- terraform-staging/bigquery/calc-oldest-allowable-freshness.sql
+++ terraform-staging/bigquery/calc-oldest-allowable-freshness.sql
@@ -3,9 +3,9 @@ RETURNS TIMESTAMP
 AS (
   CASE
     -- BigQuery Days: 1=Sunday, 2=Monday, 3=Tuesday... 7=Saturday
-    WHEN EXTRACT(DAYOFWEEK FROM timestamp) = 7 THEN TIMESTAMP_ADD(timestamp, INTERVAL - 49 HOUR) -- Saturday (48h + 1h tolerance)
-    WHEN EXTRACT(DAYOFWEEK FROM timestamp) = 1 THEN TIMESTAMP_ADD(timestamp, INTERVAL - 73 HOUR) -- Sunday (72h + 1h tolerance)
-    WHEN EXTRACT(DAYOFWEEK FROM timestamp) = 2 THEN TIMESTAMP_ADD(timestamp, INTERVAL - 73 HOUR) -- Monday (72h + 1h tolerance)
-    ELSE TIMESTAMP_ADD(timestamp, INTERVAL - 25 HOUR) -- (24h + 1h tolerance)
+    WHEN EXTRACT(DAYOFWEEK FROM timestamp) = 7 THEN TIMESTAMP_ADD(timestamp, INTERVAL - 50 HOUR) -- Saturday (48h + 2h tolerance)
+    WHEN EXTRACT(DAYOFWEEK FROM timestamp) = 1 THEN TIMESTAMP_ADD(timestamp, INTERVAL - 74 HOUR) -- Sunday (72h + 2h tolerance)
+    WHEN EXTRACT(DAYOFWEEK FROM timestamp) = 2 THEN TIMESTAMP_ADD(timestamp, INTERVAL - 74 HOUR) -- Monday (72h + 2h tolerance)
+    ELSE TIMESTAMP_ADD(timestamp, INTERVAL - 26 HOUR) -- (24h + 2h tolerance)
   END
 );
